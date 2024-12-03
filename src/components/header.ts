0f611e16c24bdf7e868c87b2c014594c298c24c8
Luke Schierer <lschiere@amazon.com>
2024-12-03 12:14:10 -0500

    updated to use the lit renderer for the header

--- src/components/header.ts
+++ src/components/header.ts
@@ -1,43 +1,53 @@
-export default class HeaderComponent extends HTMLElement {
-  connectedCallback() {
-    if (!this.shadowRoot) {
-      const template = document.createElement("template");
-      template.innerHTML = `
-        <style>
-          .header {
-            width: 100%;
-            background-color: var(--spectrum-cyan-900);
-            min-height: 30px;
-            padding-bottom: 10px;
-          }
-          .header-wrap {
-            display: flex;
-            align-items: center;
-            flex-wrap: wrap;
-          }
-          .header .brand {
-            justify-items: left;
-            padding: 10px;
-          }
-          .header .social {
-            margin-left: auto;
-            text-align: right;
-          }
-        </style>
-        <header class="header">
-          <div class="header-wrap">
-            <div class="brand">
-              <a href='/'>Project Logo Placeholder</a>
-            </div>
-            <div class="social">
-              <a href="https://github.com/lschierer/greenwood-repoductions">github</a>
-            </div>
+import { LitElement, css, html } from "lit";
+
+import SpectrumTokens from "@spectrum-css/tokens" with { type: "css" };
+
+export default class HeaderComponent extends LitElement {
+  static styles = [
+    SpectrumTokens,
+    css`
+      .header {
+        width: 100%;
+        background-color: var(--spectrum-cyan-900);
+        min-height: 30px;
+        padding-bottom: 10px;
+      }
+      .header-wrap {
+        display: flex;
+        align-items: center;
+        flex-wrap: wrap;
+      }
+      .header .brand {
+        justify-items: left;
+        padding: 10px;
+      }
+      .header .social {
+        margin-left: auto;
+        text-align: right;
+      }
+    `,
+  ];
+
+  /*
+  protected createRenderRoot() {
+    return this;
+  }
+*/
+  protected render() {
+    return html`
+      <header class="header">
+        <div class="header-wrap">
+          <div class="brand">
+            <a href="/">Project Logo Placeholder</a>
           </div>
-        </header>
-      `;
-      const shadowRoot = this.attachShadow({ mode: "open" });
-      shadowRoot.appendChild(template.content.cloneNode(true));
-    }
+          <div class="social">
+            <a href="https://github.com/lschierer/greenwood-repoductions"
+              >github</a
+            >
+          </div>
+        </div>
+      </header>
+    `;
   }
 }
 customElements.define("app-header", HeaderComponent);
