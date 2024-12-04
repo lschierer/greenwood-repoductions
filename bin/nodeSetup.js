aa0180adf363c6159b972f9b34a64af2f7856202
Luke Schierer <lschiere@amazon.com>
2024-12-04 16:05:32 -0500

    even more minimal, I am not using lit ssr, so do not need its css loader

--- bin/nodeSetup.js
+++ bin/nodeSetup.js
@@ -1,6 +1,6 @@
 import { register } from "node:module";
 import { pathToFileURL } from "node:url";
 
-register("./litCssLoader.js", pathToFileURL("./lib/"));
+//register("./litCssLoader.js", pathToFileURL("./lib/"));
 
 register("./node_modules/@greenwood/cli/src/loader.js", pathToFileURL("./"));
