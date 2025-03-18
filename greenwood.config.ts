12aba1f7369cb03c134c96c4a763708d0db63a46
Luke Schierer <lschiere@amazon.com>
2025-03-18 14:09:02 -0400

    runs in dev and build with postcss

---
+++ greenwood.config.ts
@@ -0,0 +1,26 @@
+import { greenwoodPluginPostCss } from "@greenwood/plugin-postcss";
+
+export default {
+  useTsc: true,
+  activeContent: true,
+  isolation: true,
+  optimization: "default",
+  prerender: false,
+  staticRouter: false,
+  markdown: {
+    plugins: [
+      "rehype-autolink-headings",
+      "remark-alerts",
+      "remark-gfm",
+      "remark-rehype",
+    ],
+    settings: {
+      commonmark: true,
+    },
+  },
+  plugins: [
+   greenwoodPluginPostCss({
+      extendConfig: true,
+    }),
+  ]
+};
